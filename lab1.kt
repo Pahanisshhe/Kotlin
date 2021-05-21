44408697fd5bff737aff82e9a3075725b701e75f
Pahanisshhe <72974225+Pahanisshhe@users.noreply.github.com>
2021-05-21 23:16:50 +0300

    task4

--- lab1.kt
+++ lab1.kt
@@ -2,8 +2,16 @@ fun main(){
     //task1
     print("Hello, world!")
 
-    //task2
+    //task3
     val userName = readLine();
     println("Hello, $userName")
 
+    //task4
+    println("what is your favorite programming language?")
+    when(readLine()){
+        "Prolog", "Kotlin" -> println("Подлиза!")
+        "C++" -> println("1")
+        "C#" -> println("2")
+        "Python" -> println("3")
+    }
 }
