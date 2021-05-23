ef261c35ab056e37a847d6f0d032150e26a4c064
Pahanisshhe <72974225+Pahanisshhe@users.noreply.github.com>
2021-05-23 18:38:07 +0300

    newVar

--- lab1.kt
+++ lab1.kt
@@ -1,27 +1,8 @@
-fun main(){
-    //task1
-    print("Hello, world!")
-
-    //task3
-    val userName = readLine();
-    println("Hello, $userName")
-
-    //task5
-    println("what is your favorite programming language?")
-    when(readLine()){
-        "Prolog", "Kotlin" -> println("Подлиза!")
-        "C++" -> println("1")
-        "C#" -> println("2")
-        "Python" -> println("3")
-    }
-    
-    //task6
-    sumDit("4567")
+fun main() {
+    //task1()
+    //task3()
 }
 
-fun sumDit(number :String){
-    val arr = number.toList()
-    val arrOfInt = arr.map{ it.toString().toInt() }
+fun task1() = println("Hello, world!")
 
-    print(arrOfInt.sum())
-}
+fun task3() = println("Hello, ${readLine()}!")
