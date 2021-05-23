f3efaab8ac19322e9188a18d64ec402b4d46d9c9
Pahanisshhe <72974225+Pahanisshhe@users.noreply.github.com>
2021-05-23 18:49:11 +0300

    t56

--- lab1.kt
+++ lab1.kt
@@ -1,8 +1,19 @@
 fun main() {
     //task1()
     //task3()
+    //task5()
+    //print(sumDig(45))
 }
 
 fun task1() = println("Hello, world!")
 
 fun task3() = println("Hello, ${readLine()}!")
+
+fun task5() =
+    when(readLine()){
+        "Prolog", "Kotlin" -> println("Подлиза!")
+        else -> println("(¬‿¬)")
+    }
+
+//task6
+fun sumDig(number: Int): Int = if (number == 0) 0 else sumDig(number/10) + number%10
