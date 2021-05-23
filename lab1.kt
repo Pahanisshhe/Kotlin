e1cb8c1330ace6aca6ebe116d710b97081258882
Pahanisshhe <72974225+Pahanisshhe@users.noreply.github.com>
2021-05-23 19:01:24 +0300

    t7

--- lab1.kt
+++ lab1.kt
@@ -3,6 +3,7 @@ fun main() {
     //task3()
     //task5()
     //print(sumDig(45))
+    //print(maxDig(8654))
 }
 
 fun task1() = println("Hello, world!")
@@ -17,3 +18,16 @@ fun task5() =
 
 //task6
 fun sumDig(number: Int): Int = if (number == 0) 0 else sumDig(number/10) + number%10
+
+//task7
+fun maxDig(number: Int): Int {
+    fun maxDig(number: Int, curMax: Int): Int =
+        if (number != 0)
+            if (number % 10 > curMax)
+                maxDig(number / 10, number % 10)
+            else
+                maxDig(number / 10, curMax)
+        else
+            curMax
+    return maxDig(number, 0)
+}
