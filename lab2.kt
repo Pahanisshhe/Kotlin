01e53d210b19b14d2ccef6101cbcbc50c49a16a3
Pahanisshhe <72974225+Pahanisshhe@users.noreply.github.com>
2021-05-28 20:57:50 +0300

    t2

--- lab2.kt
+++ lab2.kt
@@ -1,2 +1,7 @@
 //task1
 fun sumDigUp(number: Int): Int = if (number != 0) number%10 + sumDigUp(number/10) else 0
+
+//task2
+fun sumDigDown(number: Int, curSum: Int): Int = if (number != 0) sumDigDown(number/10, curSum + number%10) else curSum
+
+tailrec fun sumDigDownTail(number: Int, curSum: Int): Int = if (number != 0) sumDigDownTail(number/10, curSum + number%10) else curSum
