e137493d1299e746e6c837abedf0ac9a69532a45
Pahanisshhe <72974225+Pahanisshhe@users.noreply.github.com>
2021-05-28 21:20:32 +0300

    t3

--- lab2.kt
+++ lab2.kt
@@ -5,3 +5,24 @@ fun sumDigUp(number: Int): Int = if (number != 0) number%10 + sumDigUp(number/10
 fun sumDigDown(number: Int, curSum: Int): Int = if (number != 0) sumDigDown(number/10, curSum + number%10) else curSum
 
 tailrec fun sumDigDownTail(number: Int, curSum: Int): Int = if (number != 0) sumDigDownTail(number/10, curSum + number%10) else curSum
+
+//task3
+fun  mulDigUp(number: Int): Int = if (number != 0) number%10 * mulDigUp(number/10) else 1
+
+tailrec fun mulDigDown(number: Int, curMul: Int): Int = if (number != 0) mulDigDown(number/10, number%10 * curMul) else curMul
+
+fun maxDigUp(number: Int): Int =
+    if (number != 0)
+        if (number%10 > maxDigUp(number/10))
+            number%10
+        else
+            maxDigUp((number/10))
+    else 0
+
+tailrec fun maxDigDown(number: Int, maxDig: Int): Int =
+    if (number != 0)
+        if (number%10 > maxDig)
+            maxDigDown(number/10, number%10)
+        else
+            maxDigDown(number/10, maxDig)
+    else maxDig
