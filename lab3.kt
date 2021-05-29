c232ffbe98c95a0c87c42308afedb60884e76178
Pahanisshhe <72974225+Pahanisshhe@users.noreply.github.com>
2021-05-29 17:52:44 +0300

    t1

--- lab3.kt
+++ lab3.kt
@@ -1,31 +1,35 @@
 //task1
-fun inputArray(arr: Array<Int?>, length: Int): Array<Int?> =
-    if (length != 0) {
-        arr[length - 1] = readLine()?.toInt()
-        inputArray(arr, length - 1)
+fun inputArray(arr: Array<Int?>, size: Int): Array<Int?> =
+    if (size != 0) {
+
+        arr[size - 1] = readLine()?.toInt()
+        inputArray(arr, size - 1)
     }
     else arr
 
-fun outputArray(arr: Array<Int?>, length: Int): Unit? =
-    if (length != 0){
-        print("${arr[length - 1]}\t")
-        outputArray(arr, length - 1)
+fun outputArray(arr: Array<Int?>, size: Int): Unit? =
+    if (size != 0){
+        print("${arr[size - 1]}\t")
+        outputArray(arr, size - 1)
     }
     else null
 
 fun main() {
 
-    val length: Int? = readLine()?.toInt()
-    var arr: Array<Int?>? = length?.let { arrayOfNulls(it) }
-    length?.let {
+    println("Input size of array:")
+    val size: Int? = readLine()?.toInt()
+
+    println("Input array:")
+    var arr: Array<Int?>? = size?.let { arrayOfNulls(it) }
+    size?.let {
         if (arr != null) {
             val arr = inputArray(arr, it)
         }
     }
 
-    if (arr != null) {
-        if (length != null) {
-            outputArray(arr, length)
-        }
-    }
+    if (arr != null && size != null)
+        outputArray(arr, size)
+
+
+
 }
